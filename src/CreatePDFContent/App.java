f3b5c3f288445623f50778f0a0212bf758c59cc9
MMuXxin <wangmuxin2023@hotmail.com>
2026-04-18 13:59:46 +0800

    Update App.java

--- src/CreatePDFContent/App.java
+++ src/CreatePDFContent/App.java
@@ -16,27 +16,6 @@ public class App {
 
 
     }
-    @Deprecated
-    private static void method() throws IOException, ClassNotFoundException {
-        File file2 = new File("D:\\test\\[燦々SUN] 時々ボソッとロシア語でデレる隣のアーリャさん 第01巻_linked.pdf");
-        File file = new File("D:\\test\\[燦々SUN] 時々ボソッとロシア語でデレる隣のアーリャさん 第01巻.pdf");
-        File file3 =new File("D:\\test\\[閃凡人] 聖なる乙女と秘めごとを 第09巻.pdf");
-        File file4 =new File("D:\\test\\[閃凡人] 聖なる乙女と秘めごとを 第09巻_linked.pdf");
-        Scanner sc = new Scanner(System.in);
-        while (true) {
-            int s = sc.nextInt();
-            if (s==0) {
-                break;
-            }else if (s==2) {
-                //linked pdf
-                MyAddURLs.modifyPDF(file2);
-                MyAddURLs.modifyPDF(file4);
-            }else if (s==1) {
-                //unlinked pdfZ
-                MyAddURLs.modifyPDF(file);
-                MyAddURLs.modifyPDF(file3);
-            }
-        }
-        //System.out.println(file2.delete());
-    }
 }
+    
+    
